10fc271cd2f7634a43a6c63fe1d68fba80f183e2
Vedant2355 <69035408+Vedant2355@users.noreply.github.com>
2021-06-10 22:32:36 +0530

    level one new graphics

--- Scenes/GUI/lose_menu.gd
+++ Scenes/GUI/lose_menu.gd
@@ -6,8 +6,12 @@ export(PackedScene) var main_menu: PackedScene
 
 
 func _on_retry_button_up():
+	get_parent().queue_free()
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_main_menu_button_up():
+	get_parent().queue_free()
+	get_tree().paused = false
 	get_tree().change_scene_to(main_menu)
--- Scenes/GUI/pause_menu.gd
+++ Scenes/GUI/pause_menu.gd
@@ -4,12 +4,18 @@ extends Control
 export(PackedScene) var main_menu: PackedScene
 
 func _on_resume_button_up():
+	get_parent().queue_free()
+	get_tree().paused = false
 	queue_free()
 
 
 func _on_retry_button_up():
+	get_parent().queue_free()
+	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
 func _on_main_menu_button_up():
+	get_parent().queue_free()
+	get_tree().paused = false
 	get_tree().change_scene_to(main_menu)
